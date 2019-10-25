537cddccd491fd07475f32893508a00c95227bf6
Brett Kim <bkim2021@francisparker.org>
2019-10-25 13:52:54 -0700

    Finished Grayscale and Redhaze filters

--- imagejs.js
+++ imagejs.js
@@ -8,16 +8,46 @@ function upload() {
     var fileInput = document.getElementById("finput");
     canvas = document.getElementById("can");
 
-    var image = new SimpleImage(fileInput);
-    var grayImage = new SimpleImage(fileInput);
-    var redImage = new SimpleImage(fileInput);
-    var rainbowImage = new SimpleImage(fileInput);
-
-    initImages();
+    image = new SimpleImage(fileInput);
+    grayImage = new SimpleImage(fileInput);
+    redImage = new SimpleImage(fileInput);
+    rainbowImage = new SimpleImage(fileInput);
 
     image.drawTo(canvas);
 }
 
-function initImages() {
-    //Write code to grayscale and such here
+function grayscale() {
+    if(grayImage == null) {
+        alert("Image unavailable to filter");
+    }
+
+    for(var pixel of grayImage.values()) {
+        var average = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+
+        pixel.setRed(average);
+        pixel.setBlue(average);
+        pixel.setGreen(average);
+    }
+
+    canvas = document.getElementById("can");
+    grayImage.drawTo(canvas);
+}
+
+function redhaze() {
+    if(redImage == null) {
+        alert("Image unavailable to filter");
+    }
+
+    for(var pixel of redImage.values()) {
+        pixel.setBlue(0);
+        pixel.setGreen(0);
+    }
+
+    canvas = document.getElementById("can");
+    redImage.drawTo(canvas);
+}
+
+function reset() {
+    canvas = document.getElementById("can");
+    image.drawTo(canvas);
 }
