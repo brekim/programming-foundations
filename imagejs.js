029ece1c77bc9c536be09054ef1eb11e56589fe7
Brett Kim <bkim2021@francisparker.org>
2019-10-26 09:02:40 -0700

    Final project completed

--- imagejs.js
+++ imagejs.js
@@ -39,12 +39,106 @@ function redhaze() {
     }
 
     for(var pixel of redImage.values()) {
-        pixel.setBlue(0);
-        pixel.setGreen(0);
+        var average = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+
+        if(average < 128) {
+            pixel.setRed(2 * average);
+            pixel.setGreen(0);
+            pixel.setBlue(0);
+        } else {
+            pixel.setRed(255);
+            pixel.setGreen((2 * average) - 255);
+            pixel.setBlue((2 * average) - 255);
+        }
     }
 
     canvas = document.getElementById("can");
     redImage.drawTo(canvas);
+}
+
+function rainbow() {
+    if(rainbowImage == null) {
+        alert("Image unavailable to filter");
+    }
+
+    for(var pixel of rainbowImage.values()) {
+        var average = (pixel.getRed() + pixel.getGreen() + pixel.getBlue())/3;
+
+        if(pixel.getY() > (rainbowImage.getHeight() / 7)*6) {
+            if(average < 128) {
+                pixel.setRed(1.6 * average);
+                pixel.setGreen(0);
+                pixel.setBlue(1.6 * average);
+            } else {
+                pixel.setRed(0.4 * average + 153);
+                pixel.setGreen(2 * average - 255);
+                pixel.setBlue(0.4 * average + 153);
+            }
+        } else if(pixel.getY() > (rainbowImage.getHeight() / 7)*5) {
+            if(average < 128) {
+                pixel.setRed(0.8 * average);
+                pixel.setGreen(0);
+                pixel.setBlue(2 * average);
+            } else {
+                pixel.setRed(1.2 * average - 51);
+                pixel.setGreen(2 * average - 255);
+                pixel.setBlue(255);
+            }
+        } else if(pixel.getY() > (rainbowImage.getHeight() / 7)*4) {
+            if(average < 128) {
+                pixel.setRed(0);
+                pixel.setGreen(0);
+                pixel.setBlue(2 * average);
+            } else {
+                pixel.setRed(2 * average - 255);
+                pixel.setGreen(2 * average - 255);
+                pixel.setBlue(255);
+            }
+        } else if(pixel.getY() > (rainbowImage.getHeight() / 7)*3) {
+            if(average < 128) {
+                pixel.setRed(0);
+                pixel.setGreen(2 * average);
+                pixel.setBlue(0);
+            } else {
+                pixel.setRed(2 * average - 255);
+                pixel.setGreen(255);
+                pixel.setBlue(2 * average - 255);
+            }
+        } else if(pixel.getY() > (rainbowImage.getHeight() / 7)*2) {
+            if(average < 128) {
+                pixel.setRed(2 * average);
+                pixel.setGreen(2 * average);
+                pixel.setBlue(0);
+            } else {
+                pixel.setRed(255);
+                pixel.setGreen(255);
+                pixel.setBlue(2 * average - 255);
+            }
+        } else if(pixel.getY() > (rainbowImage.getHeight() / 7)*1) {
+            if(average < 128) {
+                pixel.setRed(2 * average);
+                pixel.setGreen(0.8 * average);
+                pixel.setBlue(0);
+            } else {
+                pixel.setRed(255);
+                pixel.setGreen(1.2 * average - 51);
+                pixel.setBlue(2 * average - 255);
+            }
+        } else {
+            if(average < 128) {
+                pixel.setRed(2 * average);
+                pixel.setGreen(0);
+                pixel.setBlue(0);
+            } else {
+                pixel.setRed(255);
+                pixel.setGreen(2 * average - 255);
+                pixel.setBlue(2 * average - 255);
+            }
+        }
+    }
+
+    canvas = document.getElementById("can");
+    rainbowImage.drawTo(canvas);
 }
 
 function reset() {
